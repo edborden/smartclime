47d15537263a35895f10928b7b10a287d2c6668b
edborden <borden.edward@gmail.com>
2016-07-22 12:37:59 -0400

    move to production firebase

--- config/environment.js
+++ config/environment.js
@@ -16,11 +16,11 @@ module.exports = function(environment) {
       readKey: '35eb49904f09bb75a17cfe88dbd20c674f0648b7db28bcd6b562c3d4d6b8d5703457b11ee3b11c9b3d1b210f913bef178606e6d7056923986a82caf41ccd3a650c377d916da88f68ced6449ec5cb06cff875ab79cd942e5bccef6718ceee5c0d'
     },
     firebase: {
-      apiKey: "AIzaSyCgD6qZr4p0EQErc328xdT2G2XK4Ccvihc",
-      authDomain: "smartclime-dev.firebaseapp.com",
-      databaseURL: "https://smartclime-dev.firebaseio.com",
-      storageBucket: "smartclime-dev.appspot.com"
-    }  
+      apiKey: "AIzaSyCxemskTsw23mf3_7sArXh48guU-vnDkzw",
+      authDomain: "smartclime-70e4f.firebaseapp.com",
+      databaseURL: "https://smartclime-70e4f.firebaseio.com",
+      storageBucket: "smartclime-70e4f.appspot.com"
+    }
   };
 
   if (environment === 'development') {
