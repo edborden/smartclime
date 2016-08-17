73e6a7b4d96c61c7efda2bc091efb6d67c93a161
edborden <borden.edward@gmail.com>
2016-08-17 19:13:46 -0400

    device chips with status

--- app/components/device-card/component.js
+++ app/components/device-card/component.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import HasMe from 'smartclime/mixins/has-me';
 
 const {
   Component,
   inject: { service }
 } = Ember;
 
-export default Component.extend({
+export default Component.extend(HasMe, {
 
   // attributes
   classNames: [ 'col', 's12', 'm6', 'l4', 'click-fix' ],
