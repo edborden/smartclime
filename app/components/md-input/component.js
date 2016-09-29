d690b7ddb74b5a4df8e08101c234ca1cced17afe
Ed Borden <borden.edward@gmail.com>
2016-09-29 12:24:59 -0400

    new input pattern

--- app/components/md-input/component.js
+++ app/components/md-input/component.js
@@ -1,11 +1,39 @@
-import MaterializeInputField from 'smartclime/components/md-input-field/component';
+import Ember from 'ember';
 
-export default MaterializeInputField.extend({
+const {
+  Component,
+  isPresent
+} = Ember;
+
+const MdInputComponent = Component.extend({
+  classNames: ['input-field'],
   type: 'text',
 
   didInsertElement() {
     this._super(...arguments);
     // make sure the label moves when a value is bound.
     this._setupLabel();
+  },
+
+  actions: {
+    validateProperty(changeset, property) {
+      return changeset.validate(property);
+    }
+  },
+
+  // helpers
+  _setupLabel() {
+    let $label = this.$('> label');
+    let property = this.get('property');
+    let value = this.get('changeset').get(property);
+    if (isPresent(value) && !$label.hasClass('active')) {
+      $label.addClass('active');
+    }
   }
 });
+
+MdInputComponent.reopenClass({
+  positionalParams: ['type', 'changeset', 'property']
+});
+
+export default MdInputComponent;
